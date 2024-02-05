63ae7e9a478730f3cefc31f811070bfaaff0f5c7
ahgddev <ahgddev@gmail.com>
2024-02-04 23:44:52 -0700

    Working on changing item information based on color chosen.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ let side_menu_tab = document.getElementById("side_menu_tab");
 let inner_menu = document.getElementById("inner_menu");
 let menu_close = document.getElementById("close_menu");
 let inner_menu_links = document.querySelectorAll("ul li");
+let color_dropdown = document.getElementById("color_dropdown");
+let itemStock = [
+    {color:"purple", stock: 0, price:"5.99", on_sale: [false, null]},
+    {color:"green", stock: 3, price:"3.99", on_sale: [true, "reduced"]},
+    {color:"blue", stock: 10, price:"15.99", on_sale: [true, "BOGO"]},
+    {color:"pink", stock: 8, price:"5.99", on_sale: [true, "reduced"]}
+]
 
 side_menu_tab.addEventListener("click", function(event){
     side_menu_tab.classList.add("side_slide");
@@ -20,8 +27,6 @@ menu_close.addEventListener("click",function(event){
 });
 
 for(link of inner_menu_links){
-    console.log(inner_menu_links)
-    console.log(link)
     link.addEventListener("mouseenter", function(event){
         event.currentTarget.classList.add("active");
     });
@@ -30,3 +35,34 @@ for(link of inner_menu_links){
         event.currentTarget.classList.remove("active");
     });
 }
+
+function setSaleMsg(color, sale){
+    let saleAlert = document.createElement("h1");
+    switch (sale){
+           case "reduced":
+            console.log("I'm cheap!");
+        }
+}
+
+function changeItemInfoDueToColor(){
+    let colorSelection = color_dropdown[color_dropdown.selectedIndex].value;
+    let colorStock = "";
+    let colorPrice = "";
+    let colorSale = "";
+    for(item of itemStock){
+        if(item.color == colorSelection){
+            colorStock = item.stock;
+            colorPrice = item.price;
+            colorSale = item.on_sale;
+            console.log(colorStock,colorPrice,colorSale)
+        }
+    }
+
+    setSaleMsg(colorSelection)
+}
+
+changeItemInfoDueToColor()
+
+color_dropdown.addEventListener("change", function(event){
+    changeItemInfoDueToColor();
+});
