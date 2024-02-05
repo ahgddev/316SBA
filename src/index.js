d9e8f89dd599af933a68ca97c3922803a3c1e8db
ahgddev <ahgddev@gmail.com>
2024-02-04 21:58:04 -0700

    Menu Tab slides out now.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ let side_menu_tab = document.getElementById("side_menu_tab");
 
 side_menu_tab.addEventListener("click", function(event){
     side_menu_tab.classList.add("side_slide");
+    side_menu_tab.style.left = "-100px"
 });
