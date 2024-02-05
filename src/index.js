281c76d2cd9a68586c91146ac2ab82a3b4871760
ahgddev <ahgddev@gmail.com>
2024-02-04 21:49:18 -0700

    Still working on javascript.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-let side_menu = document.getElementById("side_menu");
+let side_menu_tab = document.getElementById("side_menu_tab");
 
+side_menu_tab.addEventListener("click", function(event){
+    side_menu_tab.classList.add("side_slide");
+});
