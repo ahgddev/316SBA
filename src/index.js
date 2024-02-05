ad70e5b55e236f3120c97b5cf9b34e284b19962d
ahgddev <ahgddev@gmail.com>
2024-02-04 22:05:40 -0700

    Menu properly opens and closes.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 let side_menu_tab = document.getElementById("side_menu_tab");
+let inner_menu = document.getElementById("inner_menu");
+let menu_close = document.getElementById("close_menu");
 
 side_menu_tab.addEventListener("click", function(event){
     side_menu_tab.classList.add("side_slide");
-    side_menu_tab.style.left = "-100px"
+    side_menu_tab.style.left = "-100px";
+    inner_menu.classList.add("side_slide_in");
+    inner_menu.style.left = "30px";
+});
+
+menu_close.addEventListener("click",function(event){
+    inner_menu.classList.remove("side_slide_in");
+    inner_menu.classList.add("side_slide_out");
+    inner_menu.style.left = "-150px";
+    side_menu_tab.classList.remove("side_slide_out");
+    side_menu_tab.classList.add("side_slide_in");
+    side_menu_tab.style.left = "10px";
 });
